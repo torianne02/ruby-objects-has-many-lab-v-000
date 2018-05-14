31dff819ac42b2543866ad821bbe8029ad2d7e24
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 21:05:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,6 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1
   end
 
   def songs
@@ -15,12 +14,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count

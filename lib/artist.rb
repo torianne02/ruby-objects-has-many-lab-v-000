53307ad56e126539b79d340d1c9732c49cc49d8a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 20:41:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.song_count
-    @songs.length  
+    @songs.length
   end
 
 end

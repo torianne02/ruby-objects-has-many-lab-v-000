8fb9382a9e382801468267f94662b1ac3e98e989
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 21:02:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,8 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist
 
-  def initialize(name, genre = nil)
+  def initialize(name)
     @name = name
-    @genre = genre
   end
 
   def artist_name

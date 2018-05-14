1560f8e133c4f56a5048bdfb43cb1b029d138fdd
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 21:11:21 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -13,5 +14,17 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Author.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
   end
 end
